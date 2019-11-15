b0a52076c382a388b382bd0b827c45a4488d2d92
Brian Davenport <briandav22@gmail.com>
2019-11-15 13:21:11 -0500

    Updates

---
+++ precache-manifest.0a978d9c5abb927da838dcc05283c070.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b49975b1f85ea925710f2e983da7309",
+    "url": "/monstersrolodex/index.html"
+  },
+  {
+    "revision": "4b31d7bba2056b467cac",
+    "url": "/monstersrolodex/static/css/main.cc5bc24a.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monstersrolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "4b31d7bba2056b467cac",
+    "url": "/monstersrolodex/static/js/main.463ff2f6.chunk.js"
+  },
+  {
+    "revision": "041bbfae01e7605ae90a",
+    "url": "/monstersrolodex/static/js/runtime-main.372b9fdb.js"
+  }
+]);
